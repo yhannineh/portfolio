2cd12e44747fc21a013f13c848554f5017003f2b
Yaman <yamanhannineh@gmail.com>
2024-09-07 12:18:01 -0400

    Set up react app and created navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './navbar.css'
+//import logo from '../../assets/logo.png'
+import {Link} from 'react-scroll';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <img src="{logo}" alt="Logo" className="logo" />
+        <div className="desktopMenu">
+            <Link className="desktopMenuListItem">Home</Link>
+            <Link className="desktopMenuListItem">About</Link>
+            <Link className="desktopMenuListItem">Portfolio</Link>
+        </div>
+        <div className="contact">
+            <Link className="desktopMenuListItem">Contact</Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
