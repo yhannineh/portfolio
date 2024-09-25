e3fd9f26fd8a987d00a279f2caf38c078d3b8209
yhannineh <yamanhannineh@gmail.com>
2024-09-25 01:36:05 -0400

    Added the porfolio and pizza web links

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -10,7 +10,7 @@ const projectData = [
         icon: portfolio,
         description: "Developed a responsive portfolio website using React, featuring dynamic project cards that expand to reveal detailed information about each project. The site also includes a contact form integrated with EmailJS for easy communication.",
         technologies: ["React", "EmailJS", "CSS", "HTML", "JavaScript", "Git"],
-        url: "http://example.com"
+        url: "https://github.com/yhannineh/portfolio"
     },
     {
         id: 2,
@@ -18,7 +18,7 @@ const projectData = [
         icon: golden,
         description: "Developed a responsive pizza website with Next.js, integrating the Mailchimp API for newsletter sign-ups. Enhanced user insights and compliance by implementing Google Analytics and a cookie consent popup. Applied Playwright for end-to-end testing and used Agile principles to streamline development.",
         technologies: ["Next.js(React-based framework)", "Node.js", "HTML", "CSS", "JavaScript", "Mailchimp API", "GoogleAnalytics", "Playwright", "Git", "Agile"],
-        url: "https://is-218001-final.vercel.app/"
+        url: "https://github.com/yhannineh/IS218001-Final"
     },
     {
         id: 3,
